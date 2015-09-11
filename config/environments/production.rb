412eab7c3cfa89987a85a4dbf6be2d01251764ac
bruce <zth5906@gmail.com>
2015-09-11 17:43:17 +0800

    merge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Solar::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -32,8 +32,7 @@ Solar::Application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  # `config.assets.precompile` has moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -43,7 +42,7 @@ Solar::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -77,8 +76,8 @@ Solar::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-\
+
 end
